8a91614aa6a75f94c218a73fd0c1ef54978323df
gurnur <gurnur192@gmail.com>
2024-05-16 11:53:36 -0400

    Revert "second comment"
    This reverts commit 49c229d507e9a27006c7eec5ccf43a3913f735c7.

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -16,8 +16,6 @@ public class StudentList {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world");
-        System.out.println("Hello world2");
-        
     }
     
 }
