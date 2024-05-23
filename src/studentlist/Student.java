a9110f937d268305331b31ed58d60970b747359a
gurnur <gurnur192@gmail.com>
2024-05-23 11:46:21 -0400

    second comment

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -9,5 +9,32 @@ package studentlist;
  * @author hp
  */
 public class Student {
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStudentid(String studentid) {
+        this.studentid = studentid;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStudentid() {
+        return studentid;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    private String name;
+    private String studentid;
+    private String address;
     
 }
